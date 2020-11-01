a1f25845d822b933ac378f8c93d35953128aaba2
jonathanx111 <jonathanx111@gmail.com>
2020-11-01 18:27:13 -0500

    final

--- lib/solution.rb
+++ lib/solution.rb
@@ -1,3 +1,17 @@
+require 'pry'
 def clock_angle(time)
-    # code your solution here
+   new = time.split(":")
+   hour = new[0]
+   minute = new [1]
+   hdm = 0.5
+   hour_d = (360/12 * hour.to_i) +  (minute.to_i * hdm)
+    if hour_d == 360
+      hour_d = 0
+    end
+   minute_d = 360/60 * minute.to_i
+    difference = (hour_d - minute_d)
+    if difference < 0
+      difference = 360 - minute_d + hour_d
+    end
+    difference
 end
